e8a9d6f563585c6cfaa3f082ff22eb03f170a32e
NZBie <569019895@qq.com>
2022-07-11 09:10:19 +0800

    逻辑层框架

---
+++ BLL/BLL.cpp
@@ -0,0 +1,69 @@
+#include "BLL.h"
+
+#include "user/emailVerify.cpp"
+#include "user/doRegister.cpp"
+#include "user/doLogin.cpp"
+
+#include "user/getInfo.cpp"
+
+#include "file/getFileList.cpp"
+#include "file/newFolder.cpp"
+#include "file/deleteFile.cpp"
+
+#include "token.cpp"
+
+Json::Reader reader;
+Json::FastWriter fwriter;
+Json::StyledWriter swriter;
+
+std::map <string, bll> m_bll;
+
+void map_bll_init() {
+
+	m_bll.insert({"/user/emailVerify", &emailVerify});
+	m_bll.insert({"/user/doRegister", &doRegister});
+	m_bll.insert({"/user/doLogin", &doLogin});
+	
+	m_bll.insert({"/user/getInfo", &getInfo});
+
+	m_bll.insert({"/file/getFileList", &getFileList});
+	m_bll.insert({"/file/newFolder", &newFolder});
+	m_bll.insert({"/file/deleteFile", &deleteFile});
+}
+
+bool execute_insert(const string sql_insert) {
+
+	// 从数据库连接池获取连接
+	MYSQL* mysql = SqlConnPool::get_instance()->get_connection();
+
+	if(mysql_query(mysql, sql_insert.c_str())) {
+		// 释放连接
+		SqlConnPool::get_instance()->release_connection(mysql);
+		LOG_ERROR("MySQL select error: %s", mysql_error(mysql));
+		return false;
+	}
+
+	// 释放连接
+	SqlConnPool::get_instance()->release_connection(mysql);
+
+	return true;
+}
+
+MYSQL_RES* execute_query(const string sql_query) {
+
+	// 从数据库连接池获取连接
+	MYSQL* mysql = SqlConnPool::get_instance()->get_connection();
+
+	if(mysql_query(mysql, sql_query.c_str())) {
+		// 释放连接
+		SqlConnPool::get_instance()->release_connection(mysql);
+		LOG_ERROR("MySQL select error: %s", mysql_error(mysql));
+		return nullptr;
+	}
+
+	MYSQL_RES *result = mysql_store_result(mysql);
+	// 释放连接
+	SqlConnPool::get_instance()->release_connection(mysql);
+
+	return result;
+}
---
+++ BLL/BLL.h
@@ -0,0 +1,41 @@
+#ifndef BLL_H
+#define BLL_H
+
+#include <jsoncpp/json/json.h>
+#include <ctime>
+#include <map>
+
+#include "../log/log.h"
+#include "../MySQL_CGI/SqlConnPool.h"
+
+using Json::Value;
+
+extern Json::Reader reader;
+extern Json::FastWriter fwriter;
+extern Json::StyledWriter swriter;
+
+typedef bool (*bll)(const Value&, Value&);
+extern std::map <string, bll> m_bll;
+
+// map_bll初始化
+void map_bll_init();
+
+// 所有操作声明
+bool emailVerify(const Value& params, Value& rpsJson);	// 验证邮箱
+bool doRegister(const Value& params, Value& rpsJson);	// 注册
+bool doLogin(const Value& params, Value& rpsJson);		// 登录
+
+bool getInfo(const Value& params, Value& rpsJson);		// 获取用户信息
+
+bool getFileList(const Value& params, Value& rpsJson);	// 获取文件列表
+bool newFolder(const Value& params, Value& rpsJson); 	// 新建文件夹
+bool deleteFile(const Value& params, Value& rpsJson);	// 删除文件
+
+string generate_token(const string email);				// 生成token
+int parse_token(const string token);					// 解析token
+
+// 基本数据库操作
+bool execute_insert(const string sql_insert);
+MYSQL_RES* execute_query(const string sql_query);
+
+#endif
